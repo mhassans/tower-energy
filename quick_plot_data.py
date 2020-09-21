25fec71fd8ec6707ef88d2e2b0e9d00c2224cd8c
MohammadHassan <mhhassanshahi@yahoo.com>
2020-09-21 23:55:18 +0100

    add script for quickly plot TC and SC positions for a module

---
+++ quick_plot_data.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import math
+
+sensorCell = False #sensor cell or trigger cell
+
+u = 3
+v = 2
+layer = 36
+
+eta_min = 31  
+eta_max = 39  
+
+phi_min = 1 
+phi_max = 11  
+
+eta_ticks = [a*(2*math.pi/72) for a in range(eta_min, eta_max)] 
+phi_ticks = [b*(2*math.pi/72) for b in range(phi_min, phi_max)] 
+
+if (sensorCell):
+    cells = pd.read_csv('TCPositions/sensorCell_positions.txt', sep=' ')
+    cells.columns= ["layer","waferu","waferv","triggercellu","triggercellv","SC_eta","SC_phi"]
+else:
+    cells = pd.read_csv('TCPositions/TCPositions_Zminus_siliconOnly.csv', sep=' ')
+
+cell_u_v_layer = cells[(cells['waferu'] == u) & (cells['waferv'] == v) & (cells['layer'] == layer)]
+
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
+
+if(sensorCell):
+    plt.scatter(cell_u_v_layer['SC_eta']*-1.0, cell_u_v_layer['SC_phi'])
+else:
+    plt.scatter(cell_u_v_layer['triggercelleta']*-1.0, cell_u_v_layer['triggercellphi'])
+    
+ax.set_xticks(eta_ticks)
+ax.set_yticks(phi_ticks)
+ax.grid(which='both')
+plt.show()
+
